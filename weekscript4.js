35d018cc74510eb35f5f74515ffe27fba1c2c4ed
Sheikh Tanvir Siddiki <sheikhtanvirsiddiki8@gmail.com>
2024-10-06 12:16:50 +0600

    Update weekscript4.js

--- weekscript4.js
+++ weekscript4.js
@@ -45,7 +45,6 @@
             }
             return bengaliNumber; // Return the converted string
         }
-         window.onload = checkLogin;
         // Fetch results from Firebase
         const fetchResults = () => {
             const resultsRef = ref(db, 'results');
