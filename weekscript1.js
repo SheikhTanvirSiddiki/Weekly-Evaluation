276827a4f71a4eaf22cfb1d75930b8a7250989fb
Sheikh Tanvir Siddiki <sheikhtanvirsiddiki8@gmail.com>
2024-10-06 19:25:56 +0600

    Update weekscript1.js

--- weekscript1.js
+++ weekscript1.js
@@ -24,19 +24,6 @@ function checkPassword() {
     }
 }
 
-// Function to check if the user is already authenticated
-function checkAuthentication() {
-    const isAuthenticated = localStorage.getItem("authenticated");
-
-    // If not authenticated, redirect to the login page
-    if (!isAuthenticated) {
-        window.location.href = "index.html"; // Change this to your login page URL
-    }
-}
-
-// Call checkAuthentication on page load for all protected pages
-window.onload = checkAuthentication;
-
         // Attach the checkPassword function to the login button
         document.getElementById('login-btn').addEventListener('click', checkPassword);
     // Disable right-click
