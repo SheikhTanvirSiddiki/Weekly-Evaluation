588903144a23bfe88b8baec3eab168de0d907649
Sheikh Tanvir Siddiki <sheikhtanvirsiddiki8@gmail.com>
2024-10-06 12:24:21 +0600

    Update weekscript2.js

--- weekscript2.js
+++ weekscript2.js
@@ -1,13 +1,4 @@
-        // Function to check if user is logged in
-        function checkLogin() {
-          const isLoggedIn = sessionStorage.getItem('isLoggedIn'); // Check if user is logged in
-          if (!isLoggedIn) {
-              // Redirect to login page if not logged in
-              window.location.href = 'index.html'; // Change 'login.html' to your actual login page URL
-          }
-      }
-      window.onload = checkLogin;
-     // Disable right-click
+        // Disable right-click
         document.addEventListener('contextmenu', function(e) {
             e.preventDefault();
           });
